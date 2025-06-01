888010827bd24dcb48e0c41b4f4675a618a7e3a9
Riemsloh <olaf.lueg@t-online.de>
2025-06-01 22:37:22 +0200

    Initial Commit

---
+++ WeatherInsights/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherInsights
+//
+//  Created by Olaf Lueg on 01.06.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherInsights/WeatherInsightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherInsightsApp.swift
+//  WeatherInsights
+//
+//  Created by Olaf Lueg on 01.06.25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherInsightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
